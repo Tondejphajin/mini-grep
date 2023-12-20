2a51084b48d3457c9366cb73459cb08ddfcae133
Tin <tindejphachon@gmail.com>
2023-12-20 23:11:54 +0700

    Refactor main.rs by Splitting Code into a Library Crate

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
-use std::env::{self};
-use std::error::Error;
-use std::fs;
+use std::env;
 use std::process;
 
-struct Config {
-    query: String,
-    file_path: String,
-}
+use mini_grep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,27 +15,8 @@ fn main() {
         config.query, config.file_path
     );
 
-    if let Err(e) = run(config) {
+    if let Err(e) = mini_grep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("Contents: \n{contents}");
-
-    Ok(())
 }
