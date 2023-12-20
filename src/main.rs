0e014bd65ec17abe01071301033dd2259af5c4ac
Tin <tindejphachon@gmail.com>
2023-12-20 13:01:34 +0700

    Refactor main function to use parse_config function

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,7 @@ use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
+    let (query, file_path) = parse_config(&args);
 
     println!("Searching for: {query}, In file: {file_path}");
 
@@ -15,5 +14,10 @@ fn main() {
         }
     };
     println!("Contents: {contents}")
-
 }
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let file_path = &args[2];
+    (query, file_path)
+} 
