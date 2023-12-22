075fcf7d0c31dfc35bd17fa34f4645484d0b35ce
Tin <tindejphachon@gmail.com>
2023-12-22 14:53:52 +0700

    Add search functionality to print matching lines

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,10 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    println!("Contents: \n{contents}");
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }
